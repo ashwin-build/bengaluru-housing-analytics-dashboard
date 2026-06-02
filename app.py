4acf34112833e5a35eebff30214952ef81cd4412
Ashwin S <ashwinkumar5409@gmail.com>
2026-06-02 18:33:51 +0530

    Add Streamlit app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("🏠 Bengaluru Housing Analytics Dashboard")
+
+st.subheader("Project Overview")
+
+st.write("""
+This project analyzes Bengaluru housing data using:
+- Python
+- pandas
+- SQL
+- Data Cleaning
+- EDA
+- Feature Engineering
+""")
+
+st.metric("Average Property Price", "₹109.38 Lakhs")
+st.metric("Average Price / Sqft", "₹6596")
+
+st.success("Deployment successful 🚀")
